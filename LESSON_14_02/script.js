676ce41965c7e3e9dd08f54f2e655c4755ba6cc5
LieniteB <liene.arnicane@gmail.com>
2022-02-15 17:13:37 +0200

    14.02.2022

---
+++ LESSON_14_02/script.js
@@ -0,0 +1,19 @@
+alert("Here is my Javascript");
+alert("Hello world!");
+console.log("Here is the information added to the consol");
+console.error("here is he error console");
+console.warn("This is the warning message");
+console.clear();
+
+var a;
+let b=32;
+a=b;
+a = "Here is a string!";
+var myText = "Here is text 1";
+{
+    var myText ="Here is text 2";
+}
+console.log(myText);
+
+const CVAR = "my const var";
+console.log(CVAR)
